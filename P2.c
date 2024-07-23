85200226a7923231362c98818d0036780780ee37
Mahavir717-virda <mahavirce7@gmail.com>
2024-07-23 11:03:39 +0530

    Add files via upload

---
+++ P2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main (){
+     int a = 50;
+     int b = 30;
+int perimeter;
+     printf("%d \n", 2*(a + b));
+int area;
+     printf("%d \n", a * b);
+            printf("id : 24TCEZWT \n");
+            printf("NAME : MAHAVIR \n");
+return 0;
+}
---
+++ P5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  float celcius,farenhit;
+  printf("enter celsius : ");
+  scanf("%f",&celcius);
+  farenhit = (celcius*1.8)+32;
+  printf("farenhit is : %f \n",farenhit );
+  printf("student id : 24TCEZWT ");
+  return 0;
+}
+
