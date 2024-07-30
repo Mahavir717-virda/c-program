edff72b4bc9216f979307d5d354980e640645839
Mahavir717-virda <mahavirce7@gmail.com>
2024-07-30 09:50:14 +0530

    Add files via upload

---
+++ P9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+
+    int i;
+    printf("enter i : ");
+    scanf("%d",&i);
+    while(i)
+        {
+        printf("%d \n",i);
+        i--;
+    }
+    return 0;
+}
