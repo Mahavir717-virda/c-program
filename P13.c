1e044456e4245385c5d2e187c5b0c055b2ac0c2c
Mahavir717-virda <mahavirce7@gmail.com>
2024-08-06 09:42:19 +0530

    Add files via upload

---
+++ P13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(int a=1;a<=10;a++)
+    {
+        for(int b=1;b<=5;b++)
+        {
+            printf("%d ",a*b);
+        }
+
+    printf("\n");
+    }
+     printf(" STUDENT ID :24TCEZWT");
+     printf("NAME : Virda Mahavir");
+     return 0;
+
+}
