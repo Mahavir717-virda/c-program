3161595eb7fc8b68fad03d993010de01a87b5542
Mahavir717-virda <mahavirce7@gmail.com>
2024-08-08 15:02:35 +0530

    Add files via upload

---
+++ P15.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    for(int row=0;row<=5;row++)
+    {
+        for(int colum=4;colum>=row;colum--)
+        {
+            printf("%d ",colum);
+        }
+        printf("\n");
+    }
+    printf("U ID : 24TCEZWT \n");
+    printf("NAME : Virda Mahavir");
+    return 0;
+}
