b580c8dd5f589676d4af9881c10cc99586986214
Mahavir717-virda <mahavirce7@gmail.com>
2024-09-03 19:14:45 +0530

    Add files via upload

---
+++ P9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+
+    int i;
+    printf("enter i : ");
+    scanf("%d",&i);
+    while(i)
+    {
+        printf("%d \n",i);
+        i--;
+    }
+    printf("U-id : 24TCEZWT");
+    printf("Name : Mahavir ");
+    return 0;
+}
