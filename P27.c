245987b7e5c141d5c58da925678b75dc629cb6b1
Mahavir717-virda <mahavirce7@gmail.com>
2024-10-17 14:57:05 +0530

    Add files via upload

---
+++ P27.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int tra(int *ptr1 , int *ptr2)
+{
+    int swap;
+    for(int i=0;i<1;i++)
+    {
+         printf("Bank Balance before swapping is %d and %d\n\n",*ptr1,*ptr2);
+        swap = *ptr1;
+        *ptr1 = *ptr2;
+        *ptr2 = swap;
+        printf("Bank Balance after swapping is %d and %d\n\n",*ptr1,*ptr2);
+    }
+}
+int main()
+{
+    int Balance1 = 5000000;
+    int Balance2 = 10000000;
+    int *ptr1 = &Balance1;
+    int *ptr2 = &Balance2;
+    tra(ptr1,ptr2);
+    return 0;
+    printf("ID : 24CE142");
+    printf("NAME : Virda Mahavir");
+}
+
