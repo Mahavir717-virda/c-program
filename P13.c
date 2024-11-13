a11d17b23d5685a0cacbe6e84f32e497389b6317
Mahavir717-virda <mahavirce7@gmail.com>
2024-11-13 16:45:43 +0530

    Add files via upload

---
+++ P13.c
@@ -0,0 +1,17 @@
+//write a program to print table from 1 to 10
+#include<stdio.h>
+int main()
+{
+    for(int a=1;a<=10;a++)
+    {
+        for(int b=1;b<=5;b++)
+        {
+            printf(" \t%d * %d = %d",a,b,a*b);
+        }
+
+    printf("\n");
+    }
+     printf(" STUDENT ID :24CE142\n");
+     printf("NAME : Virda Mahavir");
+     return 0;
+}
